575b08daf9a31e3975a68c1694189da37184e19c
Samuell-Samy <samuelsamy090@gmail.com>
2025-09-05 05:32:42 +0300

    das

--- Thunder_Games/settings.py
+++ Thunder_Games/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  # your app-level static folder
-STATIC_ROOT = BASE_DIR / "staticfiles"    # deployment folder
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")   # deployment folder
 
 
 # WhiteNoise configuration for Vercel
